08c87c3146ed8aeadc74d926f1f1de3fa76acf1d
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-23 14:45:53 -0500

    add default layout with ethmarks design system

---
+++ app/layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup>
+useHead({
+    link: [
+        {
+            rel: "stylesheet",
+            href: "https://site-ethmarks.vercel.app/common/ethmarks-rich.css",
+        },
+        {
+            rel: "icon",
+            href: "https://site-ethmarks.vercel.app/common/ethmarks.ico",
+        },
+    ],
+    script: [
+        {
+            src: "https://site-ethmarks.vercel.app/common/ethmarks.wc.js",
+            defer: "true",
+        },
+    ],
+});
+</script>
+
+<template>
+    <eth-header active="projects"></eth-header>
+    <main>
+        <slot />
+    </main>
+    <eth-footer src="https://github.com/ethmarks/projects"></eth-footer>
+</template>
