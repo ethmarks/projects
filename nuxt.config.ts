e510ab4a65ded9b33e4955bb2b9df804f69a8bb8
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-23 15:34:17 -0500

    fix Nuxt removing custom elements

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,9 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag.startsWith("eth-"),
+    },
+  },
 });
