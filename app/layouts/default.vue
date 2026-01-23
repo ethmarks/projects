89f26dacb3dc8d55d29c3c5f67d9c17e0e6366c5
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-23 15:34:28 -0500

    DRY-ify default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,19 +1,26 @@
 <script setup>
+const assetsURL = "https://site-ethmarks.vercel.app/common";
+
+const title = "ethmarks/projects";
+const description = "The projects collection of ethmarks's personal website";
+const sourceURL = "https://github.com/ethmarks/projects";
+
 useHead({
     link: [
         {
-            rel: "stylesheet",
-            href: "https://site-ethmarks.vercel.app/common/ethmarks-rich.css",
+            rel: "icon",
+            href: `${assetsURL}/ethmarks.ico`,
+            type: "image/x-icon",
         },
         {
-            rel: "icon",
-            href: "https://site-ethmarks.vercel.app/common/ethmarks.ico",
+            rel: "stylesheet",
+            href: `${assetsURL}/ethmarks-rich.css`,
         },
     ],
     script: [
         {
-            src: "https://site-ethmarks.vercel.app/common/ethmarks.wc.js",
-            defer: "true",
+            src: `${assetsURL}/ethmarks.wc.js`,
+            defer: true,
         },
     ],
 });
@@ -24,5 +31,5 @@ useHead({
     <main>
         <slot />
     </main>
-    <eth-footer src="https://github.com/ethmarks/projects"></eth-footer>
+    <eth-footer :source="{ sourceURL }"></eth-footer>
 </template>
