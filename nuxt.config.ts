9e12cc85add639eb478c3e96ebaeddf8cb82fde7
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-24 14:45:36 -0500

    set Nuxt to SSR mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
-  nitro: {
-    static: true,
-  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   vue: {
