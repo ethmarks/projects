dd2ec52a2e35f4b2a34ffc89fa6812f951c00814
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-24 12:02:07 -0500

    add externalLinks.vue
    Coded with Gemini 3.0 Pro

---
+++ app/components/externalLinks.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    urls: {
+        type: Array,
+        required: true,
+        default: () => [],
+    },
+});
+
+const strategies = [
+    {
+        pattern: /github\.com/,
+        icon: "github",
+        label: "GitHub Repo",
+    },
+    {
+        pattern: /codepen\.io/,
+        icon: "codepen",
+        label: "CodePen",
+    },
+    {
+        pattern: /colab\.research\.google\.com/,
+        icon: "colab",
+        label: "Colab Notebook",
+    },
+];
+
+const defaultStrategy = {
+    icon: "webpage",
+    label: "Webpage",
+};
+
+function normalizeUrl(url) {
+    if (!url.match(/^https?:\/\//)) {
+        return `https://${url}`;
+    }
+    return url;
+}
+
+const processedLinks = computed(() => {
+    return props.urls
+        .filter((url) => url) // Filter out empty strings
+        .map((rawUrl) => {
+            const href = normalizeUrl(rawUrl);
+
+            const match = strategies.find((s) => s.pattern.test(rawUrl));
+
+            const config = match || defaultStrategy;
+
+            return {
+                href,
+                icon: config.icon,
+                label: config.label,
+            };
+        });
+});
+</script>
+
+<template>
+    <div v-if="processedLinks.length" class="external-link-row">
+        <a
+            v-for="(link, index) in processedLinks"
+            :key="index"
+            class="external-link animated"
+            :href="link.href"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <img :src="`/icons/${link.icon}.svg`" :alt="`${link.label} icon`" />
+            <span>{{ link.label }}</span>
+        </a>
+    </div>
+</template>
