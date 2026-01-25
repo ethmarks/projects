9a700f21aa01a4ac698a72f8b7e7ab600f67bd45
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-25 03:13:48 -0500

    move index card to projectCard.vue

---
+++ app/components/projectCard.vue
@@ -0,0 +1,65 @@
+<script setup>
+const props = defineProps({
+    id: {
+        type: String,
+        required: true,
+    },
+    href: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    alt: {
+        type: String,
+        required: true,
+    },
+});
+const style = `view-transition-name: img-${props.id}`;
+</script>
+
+<template>
+    <NuxtLink :to="href">
+        <img class="card" :src="props.img" :alt="props.alt" :style />
+    </NuxtLink>
+</template>
+
+<style>
+.card {
+    width: 500px;
+    height: auto;
+    max-width: 100%;
+    border-radius: var(--radius-md);
+    animation: cardEntranceLeft 1s ease forwards;
+}
+
+a {
+    width: max-content;
+}
+
+@keyframes cardEntranceLeft {
+    0% {
+        clip-path: xywh(0 0 0 30% round var(--radius-md));
+    }
+    60% {
+        clip-path: xywh(0 0 100% 40% round var(--radius-md));
+    }
+    100% {
+        clip-path: xywh(0 0 100% 100% round var(--radius-md));
+    }
+}
+
+@keyframes cardEntranceRight {
+    0% {
+        clip-path: xywh(100% 0 0 30% round var(--radius-md));
+    }
+    60% {
+        clip-path: xywh(0 0 100% 40% round var(--radius-md));
+    }
+    100% {
+        clip-path: xywh(0 0 100% 100% round var(--radius-md));
+    }
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,12 @@
 <template>
     <main>
         <h1 id="site-heading">Projects</h1>
-        <NuxtLink to="/fphf">
-            <img
-                class="card"
-                src="/fphf_konsole.webp"
-                alt="A screenshot of fphf being used in a terminal window against an abstract blue desktop wallaper. After running the command 'fphf', the terminal fills with status info as the process runs before eventually returning the string 'The SHA-256 hash of this sentence begins with b43c8b9.' and the hash 'b43c8b96f151033a566e148d45c43aa84ba153ff9407397f23d5eb43112bb5e1'"
-                width="1920"
-                height="1080"
-                style="view-transition-name: img-fphf"
-            />
-        </NuxtLink>
+        <ProjectCard
+            id="fphf"
+            href="/fphf"
+            img="/fphf_konsole.webp"
+            alt="A screenshot of fphf being used in a terminal window against an abstract blue desktop wallaper. After running the command 'fphf', the terminal fills with status info as the process runs before eventually returning the string 'The SHA-256 hash of this sentence begins with b43c8b9.' and the hash 'b43c8b96f151033a566e148d45c43aa84ba153ff9407397f23d5eb43112bb5e1'"
+        />
     </main>
 </template>
 
@@ -20,11 +16,5 @@ h1 {
     position: sticky;
     top: calc(var(--main-margin-top) + 0.2rem);
     z-index: 9999;
-}
-
-.card {
-    width: 500px;
-    height: auto;
-    max-width: 100%;
 }
 </style>
