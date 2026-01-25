d4fb76e68649b93e48b1944caf93e75dfef2282c
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-25 09:43:39 -0500

    fix fphf image flash

--- app/pages/fphf.vue
+++ app/pages/fphf.vue
@@ -6,6 +6,7 @@
         <div class="hero">
             <img
                 src="/fphf_konsole.webp"
+                decoding="sync"
                 alt="A screenshot of fphf being used in a terminal window against an abstract blue desktop wallaper. After running the command 'fphf', the terminal fills with status info as the process runs before eventually returning the string 'The SHA-256 hash of this sentence begins with b43c8b9.' and the hash 'b43c8b96f151033a566e148d45c43aa84ba153ff9407397f23d5eb43112bb5e1'"
                 width="1920"
                 height="1080"
