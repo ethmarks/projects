25c1d28682fc208e721fd83449d266e9223171e1
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-25 10:51:14 -0500

    add styles to projectCards

--- app/components/projectCard.vue
+++ app/components/projectCard.vue
@@ -16,27 +16,76 @@ const props = defineProps({
         type: String,
         required: true,
     },
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
 });
 const style = `view-transition-name: img-${props.id}`;
 </script>
 
 <template>
-    <NuxtLink :to="href">
-        <img class="card" :src="props.img" :alt="props.alt" :style />
-    </NuxtLink>
+    <div class="projectCard staggered">
+        <NuxtLink :to="href">
+            <img class="card" :src="props.img" :alt="props.alt" :style />
+        </NuxtLink>
+        <div class="info">
+            <h2>{{ props.title }}</h2>
+            <p>{{ props.desc }}</p>
+        </div>
+    </div>
 </template>
 
-<style>
-.card {
-    width: 500px;
+<style scoped>
+.projectCard {
+    --column-one: 3fr;
+    --column-two: 2fr;
+    display: grid;
+    grid-template-columns: var(--column-one) var(--column-two);
+    gap: var(--spacing-lg);
+}
+.projectCard:nth-child(2) {
+    grid-template-columns: var(--column-two) var(--column-one);
+}
+.projectCard:nth-child(2) img {
+    animation-name: cardEntranceRight;
+}
+.projectCard:nth-child(2) .info {
+    order: -1;
+    text-align: right;
+}
+.projectCard:nth-child(2) h2 {
+    animation-name: h2EntranceLeft;
+}
+h2 {
+    color: var(--color-accent);
+    animation: h2EntranceRight 1s calc(0.3s + var(--child-animation-delay) * 6)
+        ease both;
+}
+p {
+    animation: descEntrance 1s calc(0.5s + var(--child-animation-delay) * 6)
+        ease both;
+}
+
+img {
+    width: 100%;
     height: auto;
     max-width: 100%;
-    border-radius: var(--radius-md);
-    animation: cardEntranceLeft 1s 0.3s ease both;
+    object-fit: cover;
+    border-radius: var(--radius-xl);
+    border: var(--border-standard-2px);
+    animation: cardEntranceLeft 1s calc(var(--child-animation-delay) * 6) ease
+        both;
 }
 
 a {
-    width: max-content;
+    display: block;
+    width: 100%;
+    height: 100%;
 }
 
 @keyframes cardEntranceLeft {
@@ -60,6 +109,76 @@ a {
     }
     100% {
         clip-path: xywh(0 0 100% 100% round var(--radius-md));
+    }
+}
+@keyframes h2EntranceRight {
+    0% {
+        opacity: 0;
+        transform: translateX(100px);
+        text-shadow:
+            00px 0 3px color-mix(in srgb, var(--color-accent) 50%, black),
+            20px 0 3px color-mix(in srgb, var(--color-accent) 60%, black),
+            40px 0 3px color-mix(in srgb, var(--color-accent) 50%, black),
+            80px 0 3px color-mix(in srgb, var(--color-accent) 40%, black);
+    }
+
+    60% {
+        text-shadow:
+            0px 0 1px color-mix(in srgb, var(--color-accent) 50%, black),
+            1px 0 1px color-mix(in srgb, var(--color-accent) 60%, black),
+            2px 0 1px color-mix(in srgb, var(--color-accent) 50%, black),
+            4px 0 1px color-mix(in srgb, var(--color-accent) 40%, black);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translateX(0);
+        text-shadow:
+            0px 0 1px transparent,
+            1px 0 1px transparent,
+            2px 0 1px transparent,
+            4px 0 1px transparent;
+    }
+}
+@keyframes h2EntranceLeft {
+    0% {
+        opacity: 0;
+        transform: translateX(-100px);
+        text-shadow:
+            00px 0 3px color-mix(in srgb, var(--color-accent) 50%, black),
+            -20px 0 3px color-mix(in srgb, var(--color-accent) 60%, black),
+            -40px 0 3px color-mix(in srgb, var(--color-accent) 50%, black),
+            -80px 0 3px color-mix(in srgb, var(--color-accent) 40%, black);
+    }
+
+    60% {
+        text-shadow:
+            0px 0 1px color-mix(in srgb, var(--color-accent) 50%, black),
+            -1px 0 1px color-mix(in srgb, var(--color-accent) 60%, black),
+            -2px 0 1px color-mix(in srgb, var(--color-accent) 50%, black),
+            -4px 0 1px color-mix(in srgb, var(--color-accent) 40%, black);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translateX(0);
+        text-shadow:
+            0px 0 1px transparent,
+            -1px 0 1px transparent,
+            -2px 0 1px transparent,
+            -4px 0 1px transparent;
+    }
+}
+
+@keyframes descEntrance {
+    from {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
     }
 }
 </style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,16 @@
 <template>
     <main>
         <h1 id="site-heading" class="animated">Projects</h1>
-        <ProjectCard
-            id="fphf"
-            href="/fphf"
-            img="/fphf_konsole.webp"
-            alt="A screenshot of fphf being used in a terminal window against an abstract blue desktop wallaper. After running the command 'fphf', the terminal fills with status info as the process runs before eventually returning the string 'The SHA-256 hash of this sentence begins with b43c8b9.' and the hash 'b43c8b96f151033a566e148d45c43aa84ba153ff9407397f23d5eb43112bb5e1'"
-        />
+        <div class="projectCard-container">
+            <ProjectCard
+                id="fphf"
+                href="/fphf"
+                img="/fphf_konsole.webp"
+                alt="A screenshot of fphf being used in a terminal window against an abstract blue desktop wallaper. After running the command 'fphf', the terminal fills with status info as the process runs before eventually returning the string 'The SHA-256 hash of this sentence begins with b43c8b9.' and the hash 'b43c8b96f151033a566e148d45c43aa84ba153ff9407397f23d5eb43112bb5e1'"
+                title="Fixed-Point Hash Finder"
+                desc="A Rust tool that searches a cryptographic hash space for strings that contain part of their own SHA-256 hash"
+            />
+        </div>
     </main>
 </template>
 
@@ -16,5 +20,9 @@ h1 {
     position: sticky;
     top: calc(var(--main-margin-top) + 0.2rem);
     z-index: 9999;
+}
+projectCard-container {
+    display: flex;
+    flex-direction: column;
 }
 </style>
