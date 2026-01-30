ce34a1c89d8208ac6f037ad863c30c981ba414cd
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-30 15:28:00 -0500

    improve meta tag processing

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -11,47 +11,42 @@ const pageDesc = computed(
         route.meta.desc ||
         "The projects collection of ethmarks's personal website",
 );
-const pageImg = computed(() => route.meta.img || "");
+const pageImg = computed(() =>
+    route.meta.img ? `${siteURL}${route.meta.img}` : null,
+);
+const pageURL = `${siteURL}${route.path.replace(/\/$/, "")}`;
 
 useSeoMeta({
     title: pageTitle,
     ogTitle: pageTitle,
     description: pageDesc,
+    ogURL: pageURL,
     ogDescription: pageDesc,
+    ogType: "website",
+    ogImage: pageImg,
+    twitterCard: "summary_large_image",
+    twitterTitle: pageTitle,
+    twitterDescription: pageDesc,
+    twitterImage: pageImg,
 });
-
-if (pageImg.value) {
-    useSeoMeta({
-        ogImage: `${siteURL}${pageImg.value}`,
-    });
-}
 
 useHead({
     htmlAttrs: { lang: "en-US" },
     link: [
         {
             rel: "canonical",
-            href: siteURL + route.path,
+            href: pageURL,
         },
         {
             rel: "icon",
             href: `${assetsURL}/ethmarks.ico`,
             type: "image/x-icon",
         },
-        {
-            rel: "stylesheet",
-            href: `${assetsURL}/ethmarks-rich.css`,
-        },
+        { rel: "stylesheet", href: `${assetsURL}/ethmarks-rich.css` },
     ],
     script: [
-        {
-            src: `${assetsURL}/ethmarks.wc.js`,
-            defer: true,
-        },
-        {
-            src: `${assetsURL}/overflow.js`,
-            defer: true,
-        },
+        { src: `${assetsURL}/ethmarks.wc.js`, defer: true },
+        { src: `${assetsURL}/overflow.js`, defer: true },
         {
             src: `https://site-ethmarks.vercel.app/_vercel/insights/script.js`,
             defer: true,
